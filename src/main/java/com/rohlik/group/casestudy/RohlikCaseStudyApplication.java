9056813072f0e6b79f80acac23d337aa5595b752
fedir <fedichk@gmail.com>
2022-04-06 16:43:42 +0200

    Initial commit

---
+++ src/main/java/com/rohlik/group/casestudy/RohlikCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.rohlik.group.casestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RohlikCaseStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RohlikCaseStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rohlik/group/casestudy/RohlikCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohlik.group.casestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RohlikCaseStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
